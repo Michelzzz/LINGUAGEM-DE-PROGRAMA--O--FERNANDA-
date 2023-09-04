f171dd8bd30c3d855a454136c253fcc2f013131b
michelzzz <michelz.mms@gmail.com>
2023-09-04 16:10:15 -0300

    first commit

---
+++ somas.java
@@ -0,0 +1,12 @@
+public class Soma{
+
+    int n1, n2;
+    String nome;
+
+        public int media(int n1, int n2) {
+
+            int resultado = (n1 + n2) / 2;
+            return resultado;
+
+    }
+}
